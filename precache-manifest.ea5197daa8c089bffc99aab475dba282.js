eb7cf5e1d9c9fd510f7292941027bdb88fc07c87
neaRRRRR <kaancakici510@gmail.com>
2020-05-17 14:45:29 +0300

    Updates

---
+++ precache-manifest.ea5197daa8c089bffc99aab475dba282.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266bbc2a830456579f01e6e56351d73a",
+    "url": "/react-riot-api/index.html"
+  },
+  {
+    "revision": "f0e553fcd0d4b29b263e",
+    "url": "/react-riot-api/static/css/main.0bf88677.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/react-riot-api/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-riot-api/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e553fcd0d4b29b263e",
+    "url": "/react-riot-api/static/js/main.0ce34fb7.chunk.js"
+  },
+  {
+    "revision": "61da7298c3d92520a841",
+    "url": "/react-riot-api/static/js/runtime-main.69d9cfea.js"
+  },
+  {
+    "revision": "a0e43cc08695e25e00bb0e2862b19b34",
+    "url": "/react-riot-api/static/media/valorant_font-webfont.a0e43cc0.woff2"
+  },
+  {
+    "revision": "d9dafb7b723c888b6005b5e462ce361e",
+    "url": "/react-riot-api/static/media/valorant_font-webfont.d9dafb7b.woff"
+  }
+]);
